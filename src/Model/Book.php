fd244a683557d916bcd122774cb00f44db338249
akk1029 <96523229+akk1029@users.noreply.github.com>
2023-11-19 21:06:57 +0800

    fixed bug in update

--- src/Model/Book.php
+++ src/Model/Book.php
@@ -17,8 +17,16 @@ class Book {
     }
 
     public function updateBook($book_id, $title, $author, $category, $publishedYear, $quantity, $book_cover) {
-        $stmt = $this->pdo->prepare('UPDATE books SET title=?, author=?, category=?, year=?, quantity=?, available_quantity=?, image=? WHERE book_id=?)');
-        $stmt->execute([$title, $author, $category, $publishedYear, $quantity, $quantity, $book_cover, $book_id]);
+        $stmt = $this->pdo->prepare('UPDATE books SET title = :newValue1, author = :newValue2, category = :newValue3, year = :newValue4, quantity = :newValue5, available_quantity = :newValue6, image = :newValue7 WHERE book_id = :id');
+        $stmt->bindParam(':newValue1', $title);
+        $stmt->bindParam(':newValue2', $author);
+        $stmt->bindParam(':newValue3', $category);
+        $stmt->bindParam(':newValue4', $publishedYear);
+        $stmt->bindParam(':newValue5', $quantity);
+        $stmt->bindParam(':newValue6', $quantity);
+        $stmt->bindParam(':newValue7', $book_cover);
+        $stmt->bindParam(':id', $book_id);
+        $stmt->execute();
     }
 
     public function deleteBook($book_id){
