828f066d55ae973053898a350c080bc539eb6c3a
akk1029 <96523229+akk1029@users.noreply.github.com>
2023-11-21 23:00:21 +0800

    updated book_id datatype and created borrow table

--- config/database.php
+++ config/database.php
@@ -9,20 +9,35 @@ try {
     // Switch to the database
     $pdo->exec('USE library_db');
 
-    // Create a table
+    // Create tables
     $pdo->exec('
-        CREATE TABLE IF NOT EXISTS `books` (
-            `id` INT NOT NULL AUTO_INCREMENT,
-            `book_id` TEXT NOT NULL , 
-            `title` TEXT NOT NULL , 
-            `author` TEXT NOT NULL , 
-            `category` TEXT NOT NULL , 
-            `year` INT NOT NULL , 
-            `quantity` INT NOT NULL , 
-            `available_quantity` INT NOT NULL , 
-            `image` TEXT NOT NULL,
-            PRIMARY KEY (`id`) 
-        ) ENGINE = InnoDB;
+    CREATE TABLE IF NOT EXISTS `books` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `book_id` VARCHAR(255) NOT NULL, -- Change data type to VARCHAR or another text type as needed
+        `title` TEXT NOT NULL,
+        `author` TEXT NOT NULL,
+        `category` TEXT NOT NULL,
+        `year` INT NOT NULL,
+        `quantity` INT NOT NULL,
+        `available_quantity` INT NOT NULL,
+        `image` TEXT NOT NULL,
+        PRIMARY KEY (`id`),
+        UNIQUE KEY (`book_id`) -- Adding a unique constraint on book_id
+    ) ENGINE = InnoDB;
+    ');
+
+    $pdo->exec('
+    CREATE TABLE IF NOT EXISTS `borrow` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `book_id` VARCHAR(255) NOT NULL, -- Change data type to VARCHAR or another text type as needed
+        `name` TEXT NOT NULL,
+        `ph_no` INT NOT NULL,
+        `borrow_date` DATE NOT NULL,
+        `return_date` DATE NOT NULL,
+        PRIMARY KEY (`id`),
+        INDEX (`book_id`),
+        FOREIGN KEY (`book_id`) REFERENCES books(`book_id`)
+    ) ENGINE = InnoDB;
     ');
 
     require_once('config/row.php');
